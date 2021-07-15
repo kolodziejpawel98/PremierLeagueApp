6a812909a598b038744529dff2a5562a5454f138
kolodziejpawel98 <kolodziejpawel98@gmail.com>
2021-07-15 10:47:25 +0200

    pokazywanie wyniku po kolejce i druzynie

--- app.py
+++ app.py
@@ -4,43 +4,37 @@ import csv
 
 cls, wnd = uic.loadUiType('app.ui')
 
-class Nasza(wnd, cls):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
 
-    def on_pushButton_openFile_pressed(self):
-        f_name = QFileDialog.getOpenFileName(self, 'Open file')
-        self.label_fileName.setText(f_name[0]) #zmienic ta sciezke do pliku !!!!!!!!!!!!!!!!!!!!!!
-        calc_points_for_game(list_of_matches, table)
-        self.textBrowser_table.setText(str(table))
+# file = open('sezon20202021.csv', 'r')
+# reader = csv.reader(file)
 
-
-file = open('sezon20202021.csv', 'r') #????????????????????????????? zmienic ta sciezke do pliku
-reader = csv.reader(file)
-
-table = { #zmienić to na klase table z polami team i points!!!!!!!!!!!!!!!!!! 
-    "Sheffield Utd" : 0,
-    "West Brom" : 0,
-    "Fulham" : 0,
-    "Burnley" : 0,
-    "Brighton" : 0,
-    "Southampton" : 0,
-    "Crystal Palace" : 0,
-    "Wolves" : 0,
-    "Newcastle" : 0,
-    "Aston Villa" : 0,
-    "Everton" : 0,
-    "Leeds" : 0,
+table = { #zmienić to na klase table z polami team i points!!!!!!!!!!!!!!!!!!
     "Arsenal" : 0,
-    "Spurs" : 0,
-    "West Ham" : 0,
-    "Leicester" : 0,
+    "Aston Villa" : 0,
+    "Brighton" : 0,
+    "Burnley" : 0,
     "Chelsea" : 0,
+    "Crystal Palace" : 0,
+    "Everton" : 0,
+    "Fulham" : 0,
+    "Leeds" : 0,
+    "Leicester" : 0,
     "Liverpool" : 0,
-    "Man Utd" : 0,
     "Man City" : 0,
+    "Man Utd" : 0,
+    "Newcastle" : 0,
+    "Sheffield Utd" : 0,
+    "Southampton" : 0,
+    "Spurs" : 0,
+    "West Brom" : 0,
+    "West Ham" : 0,
+    "Wolves" : 0
 }
+
+print("po table")
+
+# for y in table:
+#     print(y,':',table[y])
 
 class Match:
     def __init__(self, match_num, round_num, date, location, h_team, a_team, h_goals, a_goals):
@@ -63,7 +57,7 @@ class Match:
         print("Result: " + self.h_goals + " - " + self.a_goals)
 
 
-list_of_matches = []
+# list_of_matches = []
 
 def calc_points_for_game(list_of_matches, table):
     for team in table:
@@ -77,8 +71,19 @@ def calc_points_for_game(list_of_matches, table):
         elif game.h_goals < game.a_goals:
             table[game.a_team] += 3
 
-for row in reader:
-    list_of_matches.append(Match(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7]))
+def find_game(list_of_matches, team, round):
+    for match in list_of_matches:
+        if match.h_team == team:
+            if match.round_num == str(round):
+                return match.h_team + " " + match.h_goals + " - " + match.a_goals + " " + match.a_team
+        elif match.a_team == team:
+            if match.round_num == str(round):
+                return match.h_team + " " + match.h_goals + " - " + match.a_goals + " " + match.a_team
+
+
+
+# for row in reader:
+#     list_of_matches.append(Match(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7]))
 
 # for match in list_of_matches:
 #     if match.round_num == '1':
@@ -89,6 +94,36 @@ for row in reader:
 
 
 #------------------------------------------------------------------------
+class Nasza(wnd, cls):
+
+    list_of_matches = []
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+    def on_pushButton_openFile_pressed(self):
+        f_name = QFileDialog.getOpenFileName(self, 'Open file')
+        self.label_fileName.setText(f_name[0])
+        file = open(f_name[0], 'r')
+        reader = csv.reader(file)
+
+        for row in reader:
+            self.list_of_matches.append(Match(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7]))
+
+        calc_points_for_game(self.list_of_matches, table)
+        self.textBrowser_table.setText(str(table))
+        self.spinBox_RoundNumber.setMaximum(38)
+        for team in table:
+            self.comboBox_roundTeam.addItem(team)
+
+    def on_comboBox_roundTeam_activated(self):
+        self.spinBox_RoundNumber.setValue(0)
+
+    def on_spinBox_RoundNumber_valueChanged(self):
+        self.lineEdit_roundResult.setText(find_game(self.list_of_matches, self.comboBox_roundTeam.currentText(), self.spinBox_RoundNumber.value()))
+
+
 
 
 
